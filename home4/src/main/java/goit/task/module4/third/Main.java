c179b1a8720e833f3efa59e6adafab94dc6c8513
zheka88 <zhekasonyz@gmail.com>
2016-07-24 10:37:10 +0300

    2 example instead of 2 object

--- home4/src/main/java/goit/task/module4/third/Main.java
+++ home4/src/main/java/goit/task/module4/third/Main.java
@@ -1,28 +1,26 @@
 package goit.task.module4.third;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static double findLong() {
-        return Math.sqrt(Math.pow(Point2.getX() - Point1.getX(), 2) + Math.pow(Point2.getY() - Point1.getY(), 2));
+    public static double findLong(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
     }
-
     public static void main(String[] args) throws IOException {
+        Point p1 = new Point();
+        Point p2 = new Point();
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
         System.out.println("This programm calculate the distance betwen two points.");
         System.out.println("Please enter the first point's coordinate:" + "\n" + "x");
-        Point1.setX(Integer.parseInt(input.readLine()));
+        p1.setX(Integer.parseInt(input.readLine()));
         System.out.println("y");
-        Point1.setY(Integer.parseInt(input.readLine()));
+        p1.setY(Integer.parseInt(input.readLine()));
         System.out.println("Please enter the second point's coordinate:" + "\n" + "x");
-        Point2.setX(Integer.parseInt(input.readLine()));
+        p2.setX(Integer.parseInt(input.readLine()));
         System.out.println("y");
-        Point2.setY(Integer.parseInt(input.readLine()));
-        System.out.println("The distance betwen your points = " + findLong());
+        p2.setY(Integer.parseInt(input.readLine()));
+        System.out.println("The distance betwen your points = " + findLong(p1, p2));
 
     }
 }
