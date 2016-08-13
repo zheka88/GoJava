7cebd5172ec1ffa40629c95fd6f683346a074282
zheka88 <zhekasonyz@gmail.com>
2016-08-13 21:27:48 +0300

    Third second practice

---
+++ Practice2/src/main/java/goit/task/practice2_1/Third/Main.java
@@ -0,0 +1,11 @@
+package goit.task.practice2_1.Third;
+
+public class Main {
+    public static int average(int a, int b) {
+        return (a + b) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(-4, 7));
+    }
+}
